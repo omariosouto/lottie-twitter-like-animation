239320e0b1adf40b27d03594f63b8cd51e829735
Mario Souto <soutomarios@gmail.com>
2020-08-14 17:34:44 -0300

    Update

---
+++ build/precache-manifest.7b220491335452287fb8ae3d10b42977.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b526383f67b84a7d88929c8d4c72b855",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79c6dbe6589ac913f26e",
+    "url": "/static/css/main.b63761c5.chunk.css"
+  },
+  {
+    "revision": "8a042772a284027c09d2",
+    "url": "/static/js/2.f53dd6a4.chunk.js"
+  },
+  {
+    "revision": "0d10b9de47c8023f7fd9d830b32d5242",
+    "url": "/static/js/2.f53dd6a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c6dbe6589ac913f26e",
+    "url": "/static/js/main.5533b1b9.chunk.js"
+  },
+  {
+    "revision": "b6eecc4e527e043981ca",
+    "url": "/static/js/runtime-main.cce5b797.js"
+  }
+]);
